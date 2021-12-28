1a659815cf9172677c0adb4eaff10abb3c77f8a2
Shahbaazkyz <Shahbaazkyz@gmail.com>
2021-12-28 14:17:00 +0500

    Weather API Added!

--- src/utils/weathercode.js
+++ src/utils/weathercode.js
@@ -2,7 +2,7 @@ const request = require("request");
 
 const weatherCode = (lat , lon, callback) => {
 const url = `https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&appid=a1a36875d10fc7c4646740447a2926d0&units=metric`;
-  request({ url, json: true }, (error, response) => {
+  request({ url, json: true }, (error, response) => { 
     if (error) {
       callback("Unable to connect to weather service!");
     } else if (response.body.error) {
